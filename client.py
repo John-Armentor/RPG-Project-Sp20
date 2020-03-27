3508396c2e1415a154965352c4435f826ed043a3
John Armentor <John.Armentor@usm.edu>
2020-03-27 13:23:34 -0500

    Complete rework to RPyC
    removed all previous server implementation for a move to RPyC

--- client.py
+++ client.py
@@ -13,7 +13,6 @@ sys.path.append('./player_character/abilities')
 sys.path.append('./player_character/skills')
 sys.path.append('./game_items')
 sys.path.append('./game_engine')
-sys.path.append('./netcode/')
 
 import player_character
 import character_creation
@@ -31,64 +30,31 @@ import tkinter
 import uuid
 from functools import partial
 
-from network import *
-import multiprocessing
-from _thread import *
-from FunctionPackager import FunctionPackager
+import rpyc
+import socket
 import time
 import os
 
 
-# queue of functions that are outgoing from the player to the server
-#
-client_function_queue = multiprocessing.Queue()
-
 # Multiplayer settings
 #
 ISHOST = False
-LOCALHOST = ""
+HOSTIP = ""
 
 def setIP():
-    global LOCALHOST
+    global HOSTIP
     global ISHOST
 
     selection = int(input("Enter 1 to host or 2 to join a server: "))
     
     if selection == 1:
         ISHOST = True
-        LOCALHOST = socket.gethostbyname(socket.gethostname())
+        HOSTIP = socket.gethostbyname(socket.gethostname())
+        print("You are now hosting. Your IP is: " + HOSTIP)
         
     else:
         ISHOST = False
-        LOCALHOST = input("enter an IPv4 address: ")
-
-# acts as a function for the sake of demonstrating that one can
-# be passed from client all the way to the master thread in the
-# server
-#
-def TestFunction(first_word, second_word):
-    print(first_word + " " + second_word)
-
-# acts as the master thread for the client that will process all of the data
-# in the client function queue and send it off to the server, while recieving abilities
-# updated copy of the gameboard back each time
-#
-def client_master_controller(current_network):
-    global client_function_queue
-    global table1
-    
-    while True:
-        if not client_function_queue.empty():
-            next_function_to_send = client_function_queue.get()
-            table1 = current_network.send(next_function_to_send)
-            
-
-# the function that will be used to have a function sent to the server
-# within the main game loop
-#
-def send_to_server(function, args):
-    function_to_send = FunctionPackager(function, args)
-    client_function_queue.put(function_to_send)
+        HOSTIP = input("enter an IPv4 address: ")
 
 
 # definition of Main
@@ -102,27 +68,21 @@ def main():
     setIP()
     
     if ISHOST:
-        os.system("start cmd /c server.py")
+        os.system("start cmd /c rpyc_classic.py --host 0.0.0.0")
         print("Server Launching...")
         time.sleep(3)
+        
+        print("Initializing Table...")
+        campaign_title = "The Chronicles of Testing"
+        gm1 = user.User(True)
+        host_table = tabletop.Tabletop(gm1, campaign_title)
     
-    # We initialize a network object to communicate with the server
-    #
-    current_network = Network(LOCALHOST)
-    
-    # our client side copy of the server's game table
-    #
-    table1 = current_network.get_initial_data()
-    
-    # we initialize our master controller thread
-    #
-    start_new_thread(client_master_controller,(current_network,))
-    
+    conn = rpyc.classic.connect(HOSTIP)
+    hostspace = conn.modules.sys
 
     # Main Game Loop
     #
     while run:
-        words = str(input("Say something..."))
-        send_to_server(print,(words))
+        words = input("Say something...")
         
 main()
